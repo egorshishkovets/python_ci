9f469fd7e371deb996f567150b7e817b463a176f
Egor Shishkovets <egorshishkovets@MacBook-Air-Egor.local>
2022-05-31 20:46:52 +0300

    hdbscan

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     description="A/B tools",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["hdbscan == 0.8.2",
-        "numpy >= 1.18.5", "pandas >= 1.2.4", "pydantic >= 1.8.1", "scipy >= 1.4.1",
+    install_requires=["hdbscan == 0.8.27",
+        "numpy == 1.22.0", "pandas >= 1.2.4", "pydantic >= 1.8.1", "scipy >= 1.4.1",
         "SQLAlchemy >= 1.1.15", "pytest >= 6.2", "PyYAML >= 5.4.1", "scikit_learn >= 0.24.2",
         "fastcore ==  1.4.3"
     ],
